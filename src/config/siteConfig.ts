264e643518d327e7362d893dadf7985309fa3afa
AdeleNaumann <ChocoboQJJ@gmail.com>
2026-08-04 10:34:08 +0800

    chore(deps): 更新 pnpm 及项目依赖

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -53,7 +53,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/favicon/firefly-32.png",
+			src: "/favicon/favicon.ico",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
@@ -74,9 +74,9 @@ export const siteConfig: SiteConfig = {
 		// 使用 Astro 图标库时不需要设置 valueDark，图标会自动跟随主题亮暗色切换
 		logo: {
 			type: "image",
-			value: "assets/images/logo/firefly-light.png",
-			valueDark: "assets/images/logo/firefly-dark.png",
-			alt: "🍀",
+			value: "assets/images/logo/logo.png",
+			valueDark: "assets/images/logo/logo.png",
+			alt: "ElysiumStack",
 		},
 		// 导航栏标题
 		title: "ElysiumStack",
